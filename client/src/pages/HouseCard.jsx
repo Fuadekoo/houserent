f6026165c29139e29efa3d4d0c50c5785fe9aff2
Fuadekoo <fuadabdurahman0859@gmail.com>
2024-09-24 09:45:21 +0300

    Refactor HouseCard component to improve UI and adjust dimensions

--- client/src/pages/HouseCard.jsx
+++ client/src/pages/HouseCard.jsx
@@ -9,36 +9,6 @@ import {
 } from '@material-tailwind/react';
 
 const HouseCard = ({ house }) => {
-<<<<<<< HEAD
-    return (
-        <Card className="mt-6 w-64 transform transition-transform duration-300 hover:scale-105">
-            <CardHeader color="blue-gray" className="relative h-32">
-                <img
-                    src={house.image[0]}
-                    alt="house-image"
-                    className="w-full h-full object-cover"
-                />
-            </CardHeader>
-            <CardBody>
-                <Typography variant="h5" color="blue-gray" className="mb-2">
-                    {house.address}
-                </Typography>
-                <Typography>
-                    Floor Level: {house.floorLevel}
-                </Typography>
-                <Typography>
-                    House Number: {house.houseNumber}
-                </Typography>
-                <Typography>
-                    Rent Per Month: ${house.rentPerMonth}
-                </Typography>
-            </CardBody>
-            <CardFooter className="pt-0">
-                <Button color="red">Book</Button>
-            </CardFooter>
-        </Card>
-    );
-=======
   return (
     <Card className="mt-6 w-72"> {/* Adjusted width */}
       <CardHeader color="blue-gray" className="relative h-40"> {/* Adjusted height */}
@@ -67,7 +37,6 @@ const HouseCard = ({ house }) => {
       </CardFooter>
     </Card>
   );
->>>>>>> 9f405e637ff3188e6ad18047ab3b34f416189150
 };
 
 export default HouseCard;
