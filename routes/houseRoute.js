a9d2cf13925d49cfb11b7a6acc0a01cfe4e723e3
Fuadekoo <fuadabdurahman0859@gmail.com>
2024-09-26 09:26:52 +0300

    Refactor houseRoute.js: Remove unused code and fix merge conflicts

--- routes/houseRoute.js
+++ routes/houseRoute.js
@@ -11,15 +11,11 @@ houseRouter.post('/addroom',authMiddleware,roomPostFeeMiddleware, addRoom);
 // get all houses route
 houseRouter.get('/allroom', getHouses);
 
-<<<<<<<<< Temporary merge branch 1
-houseRouter.get('/allbookedroom', getBookedHouses);
-=========
 // get single house route
 houseRouter.get('/singlehouse/:id',authMiddleware, getSingleHouse);
 
 // a house the landloard is posting
 houseRouter.get('/myaddroom',authMiddleware,ownerRoom);
 
->>>>>>>>> Temporary merge branch 2
 
 module.exports=houseRouter;
