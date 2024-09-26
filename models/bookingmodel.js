3b37d109a002b0d7a8ae060a39f75d5badee3a6a
wegegen <wegeneargow@gmail.com>
2024-09-26 10:14:47 +0300

    fix the booking model

---
+++ models/bookingmodel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  house: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'House',
+    required: true
+  },
+  bookedTime: {
+    fromTime: { 
+      type: String,
+      required: true
+    },
+    toTime: { 
+      type: String,
+      required: true
+    }
+  }
+}, {
+  timestamps: true, // This will automatically add `createdAt` and `updatedAt` fields
+  versionKey: false
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
