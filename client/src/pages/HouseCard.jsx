2b7ff7244be44ca01e53477101aafd516c22b02f
Fuadekoo <fuadabdurahman0859@gmail.com>
2024-09-26 09:22:14 +0300

    housecard is merge

--- client/src/pages/HouseCard.jsx
+++ client/src/pages/HouseCard.jsx
@@ -10,36 +10,6 @@ import {
 } from '@material-tailwind/react';
 
 const HouseCard = ({ house }) => {
-<<<<<<< HEAD
-  return (
-    <Card className="mt-6 w-72"> {/* Adjusted width */}
-      <CardHeader color="blue-gray" className="relative h-40"> {/* Adjusted height */}
-        <img
-          src={house.image[0]}
-          alt="house-image"
-          className="w-full h-full object-cover"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          {house.address}
-        </Typography>
-        <Typography>
-          Floor Level: {house.floorLevel}
-        </Typography>
-        <Typography>
-          House Number: {house.houseNumber}
-        </Typography>
-        <Typography>
-          Rent Per Month: ${house.rentPerMonth}
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button>Read More</Button>
-      </CardFooter>
-    </Card>
-  );
-=======
     return (
         <Card className="mt-6 w-64 transform transition-transform duration-300 hover:scale-105">
             <CardHeader color="blue-gray" className="relative h-32">
@@ -73,7 +43,6 @@ const HouseCard = ({ house }) => {
             </CardFooter>
         </Card>
     );
->>>>>>> dev
 };
 
 export default HouseCard;
