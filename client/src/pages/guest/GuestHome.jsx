9f22cf88bc3c6608d72cfc075780d2751fa42c05
Fuadekoo <fuadabdurahman0859@gmail.com>
2024-10-08 22:06:53 +0300

    Add Footer component to guest pages

--- client/src/pages/guest/GuestHome.jsx
+++ client/src/pages/guest/GuestHome.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Home from './Home'
+import Footer from './Footer'
 function GuestHome() {
   return <div>
     <Navbar />
   
     <Home />
+
+    <Footer />
 
   </div>
 }
